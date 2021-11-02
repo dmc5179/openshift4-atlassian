248d3a1375bb07e2223952b0f42d5c78e11f4fbd
Dan Clark <danclark@redhat.com>
2021-11-01 21:31:55 -0400

    Updates for bitbucket to use shared EFS volume

--- bitbucket/prep.sh
+++ bitbucket/prep.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the bitbucket project
-oc new-project bitbucket
+oc new-project jira
 
 # Switch to the bitbucket project
-oc project bitbucket
+oc project jira
 
 # Create the postgres database
 oc create -f ./postgres.yaml
@@ -12,15 +12,22 @@ oc create -f ./postgres.yaml
 echo "Waiting for postgres to deploy"
 sleep 15
 
+POSTGRES_POD=$(oc get pods | grep postgres | awk '{print $1}')
+oc rsh "${POSTGRES_POD}" psql -h localhost -p 5432 -U admin -p jiraadminpassword -c 'create database bibucket;'
+
 # Bitbucket needs a secret for accessing the postgresql database
 oc create secret generic postgres --from-literal=username=jirauser --from-literal=password=jirapassword
 sleep 2
 
-# Create Service Accounts and roles for bitbucket
-oc create -f ./roles.yaml
+# Create Service Accounts for bitbucket
+oc create -f ./serviceaccounts.yaml
 sleep 2
+
+# Create Role Bindings for bitbucket service accounts
+oc create -f ./rolebindings.yaml
 
 # Create persistant volumes for bitbucket
 oc create -f ./volumes.yaml
 sleep 2
 
+helm install bitbucket atlassian-data-center/bitbucket --namespace jira --values values.yaml
